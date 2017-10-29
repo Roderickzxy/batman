cb76bf964f3dc4b5bc28aa46a5b33270d72279d7
roderickzxy <roderickzxy@qq.com>
2017-10-29 23:04:48 +0800

    Initial commit

---
+++ src/main/java/com/example/batman/controller/TestController.java
@@ -0,0 +1,14 @@
+package com.example.batman.controller;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class TestController {
+
+    @RequestMapping(value="/test")
+    public String test(){
+        return "fuck you!";
+    }
+}
